70ce5bda82fab8ef6a4b584def60906a78960114
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-02-13 15:48:31 +0100

    get an empty array back

--- Controllers/FlightEmission.cs
+++ Controllers/FlightEmission.cs
@@ -1,40 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.Text;
 using System.Net.Http.Headers;
+using System.Text;
 
- public struct DepartureDate
+public class DepartureDate
 {
-public int day
-        { get; set; }
+    public int year { get; set; }
     public int month
     { get; set; }
-    public int year { get; set; }
+    public int day { get; set; }
 }
-public class Flight { 
-    public string? Origin { get; set; }
-    public string? Destination { get; set; }
-    public string? OperatingCarrierCode { get; set; }
-    public int FlightNumber { get; set; }
-    public DepartureDate departureDate { get; set; }
+public class Flight
+{
+    public string? origin { get; set; }
+    public string? destination { get; set; }
+    public string? operating_carrier_code { get; set; }
+    public int flight_number { get; set; }
+    public DepartureDate? departure_date { get; set; }
 }
 
-public class FlightList
-{
-    public List<Flight>? Flights { get; set; }
-}
 
-public class EmissionResult
-{
-    // Your code to parse the JSON response from the API
-}
 [Route("api/[controller]")]
 [ApiController]
 public class FlightController : ControllerBase
 {
     private readonly IConfiguration _configuration;
     public FlightController(IConfiguration configuration) { _configuration = configuration; }
-    
+
     [HttpGet]
     public async Task<IActionResult> GetEmissions()
     {
@@ -43,49 +35,46 @@ public class FlightController : ControllerBase
         {
             new Flight
             {
-                Origin = "ZRH",
-                Destination = "CDG",
-                OperatingCarrierCode = "AF",
-                FlightNumber= 1115,
-                departureDate= new DepartureDate {year = 2023, month = 11, day = 1}
-            },
-            new Flight
-            {
-                Origin = "CDG",
-                Destination = "BOS",
-                OperatingCarrierCode = "AF",
-                FlightNumber = 334,
-                departureDate = new DepartureDate {year = 2023, month = 11, day = 1}
-            },
-            new Flight
-            {
-                Origin = "ZRH",
-                Destination = "BOS",
-                OperatingCarrierCode = "LX",
-                FlightNumber = 54,
-                departureDate = new DepartureDate {year = 2023, month = 7, day = 1}
-            }
-        };
+                origin = "ZRH",
+                destination = "CDG",
+                operating_carrier_code = "AF",
+                flight_number= 1115,
+                departure_date=  new DepartureDate{year = 2023, month = 11,day = 1}
+            },new Flight
+            {origin="CDG",
+            destination="BOS",operating_carrier_code="AF",flight_number=334,departure_date=new DepartureDate{year=2023,month=11,day=1}}
+                 ,new Flight
+                 {origin="ZRH" ,destination="BOS",operating_carrier_code="LX",
+
+
+                flight_number=54,departure_date=new DepartureDate
+                {
+                year=2023,
+            day=1,
+            month=11
+                }
+                 } };
+
 
         var requestBody = JsonConvert.SerializeObject(new { flights });
 
         using (var httpClient = new HttpClient())
-            
+
         {
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
+            //httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
 
             var response = await httpClient.PostAsync(
                 $"https://travelimpactmodel.googleapis.com/v1/flights:computeFlightEmissions?key={apiKey}",
                 new StringContent(requestBody, Encoding.UTF8, "application/json"));
 
+            var responseBody = await response.Content.ReadAsStringAsync();
             if (response.IsSuccessStatusCode)
             {
-                var responseBody = await response.Content.ReadAsStringAsync();
                 return Ok(JsonConvert.DeserializeObject(responseBody));
             }
 
-            return BadRequest();
+            return BadRequest(JsonConvert.DeserializeObject(responseBody));
         }
     }
 }
